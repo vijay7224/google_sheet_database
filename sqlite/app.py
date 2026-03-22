8a2907e3e639d7986abadc0494d8304989e77168
vijay7224 <vijaysuryawanshi7224@gmail.com>
2026-03-22 08:00:47 +0530

    Update app.py

--- sqlite/app.py
+++ sqlite/app.py
@@ -113,4 +113,5 @@ def delete(id):
 # Run App
 # ------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 5000 fallback local testing
+    app.run(host="0.0.0.0", port=port)
